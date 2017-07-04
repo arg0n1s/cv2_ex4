47c2db4ba088e1b566662627cb2b132cfeabc9d5
Arg0n1s <basti_seppl@yahoo.com>
2017-07-04 14:50:16 +0200

    teil 1 und 2 fertig (ungetestet)

--- asgn4/code/problem1.jl
+++ asgn4/code/problem1.jl
@@ -1,14 +1,25 @@
-using Images, Optim, Grid
+using Images, Optim, Grid, Interpolations
 import PyPlot
 include("./Common.jl");
 
 function evaluate_flow(uv::Array{Float64, 3}, uv_gt::Array{Float64,3})
+  u_diff = uv[:,:,1]-uv_gt[:,:,1]
+  v_diff = uv[:,:,2]-uv_gt[:,:,2]
+  ep = sqrt(u_diff.^2 + v_diff.^2)
+  ep_filt = [x for x in ep if x != 512]
+  AEPE = sum(ep_filt)/length(ep)
 
   return AEPE::Float64
 end
 
 function warp_image(im2::Array{Float64, 2}, uv0::Array{Float64, 3})
-
+  itp = interpolate(im2, BSpline(Linear(Line())), OnGrid())
+  im_warp = zeros(Float64, size(im2))
+  for j in 1:size(im2,2)
+    for i in 1:size(im,1)
+      im_warp = itp[i+uv0[i,j,2],j+uv0[i,j,1]]
+    end
+  end
   return im_warp::Array{Float64, 2}
 end
 
@@ -29,10 +40,14 @@ end
 
 function flow_HS(im1::Array{Float64,2}, im2::Array{Float64,2}, uv0::Array{Float64,3}, lambda::Float64, sigma::Float64)
 
-  return uv_hs::::Array{Float64,3}
+  return uv_hs::Array{Float64,3}
 end
 
 function find_lambda(im1::Array{Float64,2},im2::Array{Float64,2},uv0::Array{Float64,3},uv_gt::Array{Float64,3},sigma::Float64)
 
   return lambdaMin::Float64
+end
+
+function problem1()
+
 end
