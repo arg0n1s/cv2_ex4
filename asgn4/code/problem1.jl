bcbabc68220fc4c066add3eb1d69583174a2a0f0
Nsteel <nicoacero94@gmail.com>
2017-07-04 14:50:35 +0200

    load images and flow

--- asgn4/code/problem1.jl
+++ asgn4/code/problem1.jl
@@ -12,6 +12,13 @@ function warp_image(im2::Array{Float64, 2}, uv0::Array{Float64, 3})
   return im_warp::Array{Float64, 2}
 end
 
+function load_images_and_flow()
+    img1 = 255.*channelview(float64.(Gray.(load("../data/frame10.png"))))
+    img2 = 255.*channelview(float64.(Gray.(load("../data/frame11.png"))))
+    flow10 = read_flow_file("../data/flow10.flo")
+    return img1::Array{Float64,2},img2::Array{Float64,2}, flow10::Array{Float64,3}
+end
+
 function compute_grad_images(im1::Array{Float64, 2}, im2::Array{Float64, 2}, uv0::Array{Float64,3})
 
   return Ix::Array{Float64, 2}, Iy::Array{Float64, 2}, It::Array{Float64, 2}
@@ -29,10 +36,15 @@ end
 
 function flow_HS(im1::Array{Float64,2}, im2::Array{Float64,2}, uv0::Array{Float64,3}, lambda::Float64, sigma::Float64)
 
-  return uv_hs::::Array{Float64,3}
+  return uv_hs::Array{Float64,3}
 end
 
 function find_lambda(im1::Array{Float64,2},im2::Array{Float64,2},uv0::Array{Float64,3},uv_gt::Array{Float64,3},sigma::Float64)
 
   return lambdaMin::Float64
+end
+
+
+function problem1()
+  im1, im3, flow10 = load_images_and_flow()
 end
